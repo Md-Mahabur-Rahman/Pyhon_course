ea34e5331dc94dbf23359e5def0576d72b5a3ef3
Md-Mahabur-Rahman <43383939+Md-Mahabur-Rahman@users.noreply.github.com>
2024-10-31 11:13:09 +0000

    Now Update

---
+++ Python/pythonsame/example3.py
@@ -0,0 +1,5 @@
+numbers =[2,3,4,5,6,7,8,9,20]
+even_set ={num for num in numbers if num % 2==0}
+print('Set of Even Numbers : ', even_set)
+
+
---
+++ Python/pythonsame/example6.py
@@ -0,0 +1,14 @@
+set1={2,3,4,5}
+set2={4,5,6,7}
+set3={6,7,8}
+union_result =set1|set2|set3
+intersection_result =set1& set2& set3
+symmetric_difference_result =set1^ set2^ set3
+
+
+print("Union of Sets : ",union_result)
+print('Intersection of sets :',intersection_result)
+print("Symmetric Difference of Sets : ",symmetric_difference_result)
+
+
+
